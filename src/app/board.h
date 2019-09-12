5871f45838353faadce6bfcf848768f5bdb2a4f0
Francois Berder <18538310+francois-berder@users.noreply.github.com>
2019-09-12 10:56:59 +0100

    Move pins definitions to board.h
    Also define clock speeds
    
    Signed-off-by: Francois Berder <18538310+francois-berder@users.noreply.github.com>

---
+++ src/app/board.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2019 Francois Berder <fberder@outlook.fr>
+ *
+ * This file is part of waterpump.
+ *
+ * waterpump is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * waterpump is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with waterpump.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "mcu/gpio.h"
+
+#define ENABLE_GSM_PIN      (GPIO_PIN(PORT_B, 7))
+#define ENABLE_PUMP1_PIN    (GPIO_PIN(PORT_B, 6))
+#define ENABLE_PUMP2_PIN    (GPIO_PIN(PORT_B, 5))
+#define LED_PIN             (GPIO_PIN(PORT_A, 7))
+#define REQ_WATER_PIN       (GPIO_PIN(PORT_A, 5))
+
+#define CORE_CLOCK          (16000000)
+#define CLOCK_APB1          (16000000)
+#define CLOCK_APB2          (16000000)
+#define CLOCK_LSI           (38000)
+
+#endif
